ba51321677ccb33d80ef753194f236eb99358b4c
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 08:56:56 -0600

    Update extensions.py
    creating load directory method

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -6,3 +6,7 @@ def load_module(self, directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
+
+def load_directory(self, directory):
+    for directory in path.__iter__(Path.rglob(".py")):
+        load_module(directory.as_posix(), path.stem)
