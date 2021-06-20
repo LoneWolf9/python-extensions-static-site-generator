b4c9cae57d990d830a71c40a0e7952a6e4c4962e
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 09:40:07 -0600

    Update menu.py
    updating syntax

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,6 +6,6 @@ files = []
 def collect_files(source, site_parsers):
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in site_parsers.list(filter(valid, site_parsers)):
+        for parser in list(filter(valid, site_parsers)):
             if path.suffix.parser.valid_file_ext():
                 path.append(files)
