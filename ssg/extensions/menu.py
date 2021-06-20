f82402a4df86315de4cecbef9e559d38a904d349
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 09:39:32 -0600

    Update menu.py
    updating collect_files

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -5,3 +5,7 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
+    for path in source.rglob("*"):
+        for parser in site_parsers.list(filter(valid, site_parsers)):
+            if path.suffix.parser.valid_file_ext():
+                path.append(files)
