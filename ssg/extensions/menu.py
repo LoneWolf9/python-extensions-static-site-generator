c9b379bdaab0f49974aaeda6aeab926c55570198
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 09:29:24 -0600

    Update menu.py
    creating collect files function

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,3 +1,7 @@
 from ssg import hooks, parsers
 
 files = []
+
+@hooks.register("collect_files")
+def collect_files(self, source, site_parsers):
+    valid = lambda p : isinstance() not parsers.ResourceParser()
