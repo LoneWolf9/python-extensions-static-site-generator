d57d5d92e5df7c3e831a27257e4f44d623d1f56c
LoneWolf9 <lstdon9@yahoo.com>
2021-06-20 09:11:05 -0600

    Update hooks.py
    updating register callback method

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -2,5 +2,6 @@ _callbacks = {}
 
 def register(hook, order=0):
     def register_callback(func):
+        _callbacks.setdefault(hook, {}).setdefaut(order, []).append(func)
         return func
     return register_callback
