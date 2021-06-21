6bb290bcd8e7efce3450c605de036f3e794237c1
LoneWolf9 <lstdon9@yahoo.com>
2021-06-21 11:25:34 -0600

    Update site.py
    adding hooks events to the build function

--- ssg/site.py
+++ ssg/site.py
@@ -24,18 +24,21 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             self.error(
-                "No parser for the {} extension, file skipped!".format(path.suffix)
+                "No parser for the {} extension, file skipped!".format(
+                    path.suffix)
             )
 
     def build(self):
         extensions.load_bundled()
-        hooks.event("collect_files",self.source, self.parsers)
+        hooks.event("collect_files", self.source, self.parsers)
+        hooks.event("start_build")
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+        hooks.event("stats")
 
     @staticmethod
     def error(message):
